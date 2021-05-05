f343d82a3c16bc4fc79ad0e9f0d47988af929c96
mohammad <m.zandieh1999@gmail.com>
2021-05-05 23:22:11 +0430

    add Manifest Tags

--- app/src/main/java/com/example/broadcastreceiverwithmanifest/MyBroadcastreceiver.java
+++ app/src/main/java/com/example/broadcastreceiverwithmanifest/MyBroadcastreceiver.java
@@ -3,10 +3,13 @@ package com.example.broadcastreceiverwithmanifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class MyBroadcastreceiver extends BroadcastReceiver {
+    private static final String TAG = "MyBroadcastreceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        Log.i(TAG, "onReceive: " + intent.getAction());
     }
 }
