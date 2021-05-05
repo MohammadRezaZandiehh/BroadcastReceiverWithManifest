14ae9eaf784edaa7eb4e88a01a5b192040ce9834
mohammad <m.zandieh1999@gmail.com>
2021-05-05 23:13:02 +0430

    add MyBroadcastReceiver class

---
+++ app/src/main/java/com/example/broadcastreceiverwithmanifest/MyBroadcastreceiver.java
@@ -0,0 +1,12 @@
+package com.example.broadcastreceiverwithmanifest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyBroadcastreceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
